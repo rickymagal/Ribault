360f0dfc76b733b99d6ff60ec3146feb1f04ed5f
rickymagal <ricardofilhoschool@gmail.com>
2025-06-11 17:12:24 -0300

    feat: Supers implemented and linked to c

---
+++ src/Lib/Supers_stub.h
@@ -0,0 +1,11 @@
+#include <HsFFI.h>
+#if defined(__cplusplus)
+extern "C" {
+#endif
+extern void super1(HsInt32 a1, HsPtr a2, HsPtr a3);
+extern void super2(HsInt32 a1, HsPtr a2, HsPtr a3);
+extern void super3(HsInt32 a1, HsPtr a2, HsPtr a3);
+#if defined(__cplusplus)
+}
+#endif
+
