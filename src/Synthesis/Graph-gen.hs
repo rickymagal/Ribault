c9ab7b1de93c692fd0bc50876b5824cd888561ae
rickymagal <ricardofilhoschool@gmail.com>
2025-06-14 00:34:44 -0300

    fix: graph gen almost there

--- src/Synthesis/Graph-gen.hs
+++ src/Synthesis/Graph-gen.hs
@@ -10,19 +10,22 @@ import           Data.Char      (isAlphaNum)
 import           Syntax         (Program(..), Decl(..), Expr(..), Literal(..), Ident, BinOperator(..), Pattern(..))
 import           Semantic       (desugarProgram)
 import           Control.Monad.State
+import           Control.Monad (replicateM, forM, forM_, zipWithM)
 import           Data.List (intercalate)
+import           Data.Maybe (catMaybes)
 
-formatReg :: String -> String
-formatReg name
-  | "const" `prefixOf` name = name
-  | "t"     `prefixOf` name = "temp" ++ drop 1 name
-  | otherwise               = name
-  where prefixOf p s = take (length p) s == p
-
-data CGCtx = CGCtx { counter :: Int, nodes :: [Text], edges :: [Text], env :: [(Ident, String)], defs :: [(Ident, ([Ident], Expr))] }
+-- | Contexto com contador, nós, arestas e ambiente de variáveis e defs
+data CGCtx = CGCtx
+  { counter :: Int
+  , nodes   :: [Text]
+  , edges   :: [Text]
+  , env     :: [(Ident, String)]
+  , defs    :: [(Ident, ([Ident], Expr))]
+  }
 
 type GenM = State CGCtx
 
+-- | Gera novo identificador único
 freshId :: String -> GenM String
 freshId base = do
   st <- get
@@ -30,22 +33,21 @@ freshId base = do
   put st { counter = n + 1 }
   pure (base ++ show n)
 
+-- | Gera nó Graphviz
 emitNode :: String -> String -> GenM String
 emitNode label body = do
   name <- freshId label
-  let formatted = formatReg name
-  modify (\s -> s { nodes = nodes s ++ [T.pack (formatted ++ " [label=\"{" ++ formatted ++ "|" ++ escape body ++ "}\"]") ] })
-  pure formatted
-  where escape = concatMap escapeChar
-        escapeChar '"' = "\\\""
-        escapeChar c   = [c]
+  modify $ \s -> s { nodes = nodes s ++ [T.pack $ name ++ " [label=\"" ++ body ++ "\", shape=box, style=rounded];"] }
+  pure name
 
+-- | Gera aresta Graphviz
 emitEdge :: String -> String -> GenM ()
 emitEdge from to =
-  modify (\s -> s { edges = edges s ++ [T.pack (formatReg from ++ " -> " ++ formatReg to)] })
+  modify $ \s -> s { edges = edges s ++ [T.pack $ from ++ " -> " ++ to ++ ";"] }
 
+-- | Vincula identificador a registrador
 bindVar :: Ident -> String -> GenM ()
-bindVar name reg = modify (\s -> s { env = (name, reg) : env s })
+bindVar name reg = modify $ \s -> s { env = (name, reg) : env s }
 
 lookupVar :: Ident -> GenM String
 lookupVar name = do
@@ -55,18 +57,19 @@ lookupVar name = do
     Nothing -> pure name
 
 bindDef :: Ident -> [Ident] -> Expr -> GenM ()
-bindDef f ps e = modify (\s -> s { defs = (f, (ps, e)) : defs s })
+bindDef f ps e = modify $ \s -> s { defs = (f, (ps, e)) : defs s }
 
 lookupDef :: Ident -> GenM (Maybe ([Ident], Expr))
 lookupDef f = do
   st <- get
   pure (lookup f (defs st))
 
+-- | Entrada principal: gera .dot
 programToDataflowDot :: Program -> Text
 programToDataflowDot prog =
   let Program decls = desugarProgram prog
       CGCtx _ ns es _ _ = execState (mapM_ recordDecl decls >> mapM_ genDecl decls) (CGCtx 0 [] [] [] [])
-  in T.unlines ( ["digraph Dataflow {", "  node [shape=record,fontname=Courier];"] ++ ns ++ es ++ ["}"])
+  in T.unlines (["digraph G {"] ++ ns ++ es ++ ["}"])
 
 recordDecl :: Decl -> GenM ()
 recordDecl (FunDecl name ps body) = bindDef name ps body
@@ -74,7 +77,7 @@ recordDecl (FunDecl name ps body) = bindDef name ps body
 genDecl :: Decl -> GenM ()
 genDecl (FunDecl name [] body) = do
   res <- genExpr [] body
-  r <- emitNode "ret" ("ret " ++ name ++ ", " ++ res)
+  r <- emitNode "ret" ("ret " ++ name)
   emitEdge res r
   pure ()
 genDecl (FunDecl _ (_:_) _) = pure ()
@@ -84,44 +87,39 @@ genExpr stack = \case
   Var x -> lookupVar x >>= emitNode "var"
 
   Lit l -> case l of
-    LInt i   -> emitNode "const" ("const " ++ show i)
-    LFloat f -> emitNode "fconst" ("fconst " ++ show f)
-    LBool b  -> emitNode "const" ("const " ++ if b then "1" else "0")
-    LChar c  -> emitNode "const" ("const " ++ show (fromEnum c))
-    LString s -> do
-      cs <- mapM (\ch -> emitNode "char" ("const " ++ show (fromEnum ch))) s
-      sn <- emitNode "super" ("super, 1, " ++ show (length cs))
-      mapM_ (\c -> emitEdge c sn) cs
-      pure sn
+    LInt i   -> emitNode "const" ("const #" ++ show i)
+    LBool b  -> emitNode "const" ("const #" ++ if b then "1" else "0")
+    _        -> emitNode "const" "const #?"
 
   App f x -> do
     let flatten (App f1 x1) = let (fn, args) = flatten f1 in (fn, args ++ [x1])
         flatten e = (e, [])
     let (f', args) = flatten (App f x)
-
-    fname <- case f' of Var n -> pure n; _ -> error "Only Var application supported"
-    mdef <- lookupDef fname
-    case mdef of
-      Just (params, body) ->
-        if fname `elem` stack
-          then fallbackToCall fname args stack
-          else do
-            argRegs <- mapM (genExpr stack) args
-            oldEnv <- gets env
-            mapM_ (uncurry bindVar) (zip params argRegs)
-            res <- genExpr (fname:stack) body
-            modify (\s -> s { env = oldEnv })
-            pure res
-      Nothing -> fallbackToCall fname args stack
+    case f' of
+      Var fid -> do
+        mdef <- lookupDef fid
+        case mdef of
+          Just (params, body) ->
+            if fid `elem` stack
+              then fallbackToCall fid args stack
+              else do
+                argRegs <- mapM (genExpr stack) args
+                oldEnv <- gets env
+                mapM_ (uncurry bindVar) (zip params argRegs)
+                res <- genExpr (fid : stack) body
+                modify $ \s -> s { env = oldEnv }
+                pure res
+          Nothing -> fallbackToCall fid args stack
+      _ -> fallbackToCall "anon" (f' : args) stack
 
   BinOp op a b -> do
     ra <- genExpr stack a
     rb <- genExpr stack b
     let opname = case op of
           Add -> "add"; Sub -> "sub"; Mul -> "mul"; Div -> "div"; Mod -> "mod"
-          Eq -> "equal"; Neq -> "notequal"; Lt -> "lthan"; Le -> "lthani"
-          Gt -> "gthan"; Ge -> "gthani"; And -> "and"; Or -> "or"
-    n <- emitNode opname (opname ++ " result, " ++ ra ++ ", " ++ rb)
+          Eq -> "eq"; Neq -> "neq"; Lt -> "lt"; Le -> "le"
+          Gt -> "gt"; Ge -> "ge"; And -> "and"; Or -> "or"
+    n <- emitNode opname opname
     emitEdge ra n >> emitEdge rb n
     pure n
 
@@ -129,67 +127,48 @@ genExpr stack = \case
     rc <- genExpr stack c
     rt <- genExpr stack t
     re <- genExpr stack e
-    n <- emitNode "steer" ("steer result, " ++ rc ++ ", " ++ rt ++ ", " ++ re)
+    n <- emitNode "steer" "steer"
     emitEdge rc n >> emitEdge rt n >> emitEdge re n
     pure n
 
   Let decls body -> do
-    mapM_ (\(FunDecl x [] e) -> do r <- genExpr stack e; bindVar x r) decls
+    mapM_ (\(FunDecl x [] e) -> genExpr stack e >>= bindVar x) decls
     genExpr stack body
 
   Tuple es -> do
     rs <- mapM (genExpr stack) es
-    n <- emitNode "super" ("super, 1, " ++ show (length rs))
-    mapM_ (\r -> emitEdge r n) rs
+    n <- emitNode "super" ("S" ++ show (length rs))
+    mapM_ (`emitEdge` n) rs
     pure n
 
   List es -> do
     rs <- mapM (genExpr stack) es
-    n <- emitNode "super" ("super, 1, " ++ show (length rs))
-    mapM_ (\r -> emitEdge r n) rs
+    n <- emitNode "super" ("S" ++ show (length rs))
+    mapM_ (`emitEdge` n) rs
     pure n
 
-  Lambda{} -> emitNode "lambda" "// lambda (ignored)"
+  Lambda{} -> emitNode "lambda" "lambda"
 
   Case e alts -> do
     scrut <- genExpr stack e
-    branches <- forM alts $ \(pat, expr) -> do
-      oldEnv <- gets env
-      cond <- case pat of
-        PVar v -> bindVar v scrut >> pure scrut
-        PLit l -> do
-          lit <- genExpr stack (Lit l)
-          cmp <- emitNode "equal" ("equal result, " ++ scrut ++ ", " ++ lit)
-          emitEdge scrut cmp >> emitEdge lit cmp
-          pure cmp
-        PWildcard -> pure scrut
-        PTuple ps -> do
-          parts <- replicateM (length ps) (freshId "pt")
-          splitter <- emitNode "split" ("splitn " ++ unwords parts ++ ", " ++ scrut)
-          emitEdge scrut splitter
-          mapM_ (\p -> emitEdge splitter p) parts
-          let binders = [(v, r) | (PVar v, r) <- zip ps parts]
-          mapM_ (uncurry bindVar) binders
-          pure scrut
-        _ -> error "Unsupported pattern"
-      val <- genExpr stack expr
-      modify (\s -> s { env = oldEnv })
-      pure (cond, val)
-    let (conds, results) = unzip branches
-    steer <- emitNode "steer" ("steer result, " ++ scrut ++ concatMap (", " ++) results)
-    emitEdge scrut steer
-    mapM_ (\r -> emitEdge r steer) results
-    pure steer
+    altResults <- forM alts $ \(pat, expr) -> case pat of
+      PVar x -> do
+        bindVar x scrut
+        r <- genExpr stack expr
+        pure r
+      _ -> genExpr stack expr
+    n <- emitNode "steer" "steer"
+    mapM_ (`emitEdge` n) (scrut : altResults)
+    pure n
 
 fallbackToCall :: Ident -> [Expr] -> [Ident] -> GenM String
 fallbackToCall fname args stack = do
   argRegs <- mapM (genExpr stack) args
-  callgroup <- emitNode "cg" ("callgroup(\"cg_" ++ fname ++ "\", \"" ++ fname ++ "\")")
+  callgroup <- emitNode "callgroup" ("callgroup(" ++ fname ++ ")")
   zipWithM_ (\i a -> do
-    n <- emitNode "snd" ("callsnd " ++ fname ++ "[" ++ show (i+1) ++ "], " ++ a ++ ", cg_" ++ fname)
-    emitEdge a n
-    emitEdge n callgroup) [0..] argRegs
-  retreg <- freshId "rtsnd"
-  nret <- emitNode "retsnd" ("retsnd " ++ fname ++ "[0], " ++ retreg ++ ", cg_" ++ fname)
-  emitEdge callgroup nret
-  pure retreg
+    snd <- emitNode "callsnd" ("callsnd(" ++ fname ++ ", " ++ show i ++ ")")
+    emitEdge a snd
+    emitEdge snd callgroup) [1..] argRegs
+  r <- emitNode "retsnd" ("retsnd(" ++ fname ++ ")")
+  emitEdge callgroup r
+  pure r
